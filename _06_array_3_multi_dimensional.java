978fe803ec6c5ad5c720275216fba79e08b5fcd6
Jose Santos <airamez@gmail.com>
2023-11-23 00:51:08 -0800

    Add content for Arrays multidimensionals

--- _06_array_3_multi_dimensional.java
+++ _06_array_3_multi_dimensional.java
@@ -1,3 +1,62 @@
 public class _06_array_3_multi_dimensional {
-    
+    public static void main (String[] args) {
+        int[][] biDimensional = {
+            {10, 11, 12, 13, 14},
+            {15, 16, 17, 19, 19},
+            {20, 21, 22, 23, 24},
+            {25, 26, 27, 28, 29},
+        };
+        printArrayBiDimensional(biDimensional);
+
+        int[][][] triDimensional = {
+            {
+                {10, 11, 12, 13, 14},
+                {15, 16, 17, 19, 19},
+                {20, 21, 22, 23, 24},
+                {25, 26, 27, 28, 29},
+            },
+            {
+                {30, 31, 32, 33, 34},
+                {35, 36, 37, 39, 49},
+                {40, 41, 42, 43, 44},
+                {45, 46, 47, 48, 49},
+            },
+            {
+                {50, 51, 52, 53, 54},
+                {55, 56, 57, 58, 59},
+                {60, 61, 62, 63, 64},
+                {65, 66, 67, 68, 69},
+            },
+            {
+                {70, 71, 72, 73, 74},
+                {75, 76, 77, 78, 79},
+                {80, 81, 82, 83, 84},
+                {85, 86, 87, 88, 89},
+            }
+        };
+        printArrayTriDimensional(triDimensional);
+    }
+
+    public static void printArrayBiDimensional(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(String.format("%s ", array[i][j]));
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void printArrayTriDimensional(int[][][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+                    System.out.print(String.format("%s ", array[i][j][k]));
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
---
+++ _06_array_4_multi_dimensional.java
@@ -0,0 +1,26 @@
+public class _06_array_4_multi_dimensional {
+    public static void main (String[] args) {
+        String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "Octorber", "Nocvember", "December"};
+        int[] preciptationPerMonth = {5, 3, 6, 2, 1, 0, 0, 8, 10, 5, 3, 6};
+        // Printing precipatin per month
+        for (int i = 0; i < preciptationPerMonth.length; i++) {
+            System.out.printf("%s= %s\n", months[i], preciptationPerMonth[i]);
+        }
+
+        String[] states = {"California", "Florida", "Nevada", "Texas"};
+        int[][] preciptationPerMonthPerState = {
+            {11, 13, 16, 15, 10, 10, 15, 18, 10, 15, 10, 12},
+            {19, 21, 25, 22, 31, 35, 50, 38, 20, 45, 25, 24},
+            {10, 11, 12, 12, 15, 20, 11, 12, 15, 17, 15, 18},
+            {11, 11, 13, 11, 21, 13, 10, 15, 14, 13, 15, 27},
+        };
+        
+        System.out.println("Precipitation Per State/Month report:");
+        for (int i = 0; i < states.length; i++) {
+            System.out.printf("State: %s\n", states[i]);
+            for (int j = 0; j < preciptationPerMonthPerState[i].length; j++) {
+                System.out.printf("  %s= %s\n", months[j], preciptationPerMonthPerState[i][j]);
+            }
+        }
+    }
+}
